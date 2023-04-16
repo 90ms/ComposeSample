55101afd46f4cad38702ab00d4115c1c2578205b
90MS <myungsung510@gmail.com>
2023-04-16 21:11:07 +0900

    add baseEffect callback

--- library/design/src/main/java/k90ms/compose/design/ArchitectureHelper.kt
+++ library/design/src/main/java/k90ms/compose/design/ArchitectureHelper.kt
@@ -1,11 +1,48 @@
 package k90ms.compose.design
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 
 interface BaseUnidirectionalViewModel<BASE_EVENT, BASE_EFFECT, BASE_STATE> {
     val baseState: StateFlow<BASE_STATE>
     val baseEffect: Flow<BASE_EFFECT>
 
     fun baseEvent(event: BASE_EVENT)
+}
+
+@Composable
+inline fun <reified BASE_EVENT, BASE_EFFECT, BASE_STATE> useBase(
+    viewModel: BaseUnidirectionalViewModel<BASE_EVENT, BASE_EFFECT, BASE_STATE>,
+): StateEffectDispatch<BASE_EVENT, BASE_EFFECT, BASE_STATE> {
+
+    val state by viewModel.baseState.collectAsStateWithLifecycle()
+
+    val dispatch: (BASE_EVENT) -> Unit = { event->
+        viewModel.baseEvent(event)
+    }
+
+    return StateEffectDispatch(
+        state = state,
+        effectFlow = viewModel.baseEffect,
+        dispatch = dispatch
+    )
+}
+
+data class StateEffectDispatch<EVENT, EFFECT, STATE>(
+    val state: STATE,
+    val effectFlow: Flow<EFFECT>,
+    val dispatch: (EVENT) -> Unit,
+)
+
+@Composable
+fun <T>Flow<T>.collectInLaunchedEffect(function: suspend (value: T) -> Unit ){
+    val flow = this
+    LaunchedEffect(key1 = flow) {
+        flow.collectLatest(function)
+    }
 }
--- library/design/src/main/java/k90ms/compose/design/base/BaseScreen.kt
+++ library/design/src/main/java/k90ms/compose/design/base/BaseScreen.kt
@@ -1,9 +1,13 @@
 package k90ms.compose.design.base
 
+import android.app.Activity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import k90ms.compose.design.collectInLaunchedEffect
+import k90ms.compose.design.useBase
 
 @Composable
 fun BaseRoute(
@@ -11,7 +15,21 @@ fun BaseRoute(
     content: @Composable () -> Unit,
 ) {
 
+    val (baseState, baseEffect, baseEvent) = useBase(viewModel = baseViewModel)
+
+    val context = LocalContext.current
+    val activity = content as? Activity
+
+    baseEffect.collectInLaunchedEffect {
+        when(it) {
+            BaseContract.BaseEffect.OnBackPressed -> {
+                activity?.onBackPressed()
+            }
+        }
+    }
+
     BaseScreen(
+        baseState = baseState,
         content = content,
     )
 }
@@ -19,10 +37,13 @@ fun BaseRoute(
 
 @Composable
 private fun BaseScreen(
+    baseState: BaseContract.BaseState,
     content: @Composable () -> Unit,
 ) {
 
     Box(modifier = Modifier.fillMaxSize()) {
-        content()
+        when(baseState) {
+            BaseContract.BaseState.OnSuccess -> content()
+        }
     }
 }
